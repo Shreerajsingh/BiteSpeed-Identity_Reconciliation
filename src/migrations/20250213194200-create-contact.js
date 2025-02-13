b276192ba1796a8721aab0d5f838784436fae407
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2025-02-14 01:34:09 +0530

    Added contact model using sequelize

---
+++ src/migrations/20250213194200-create-contact.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      linkedId: {
+        type: Sequelize.INTEGER
+      },
+      linkPrecedence: {
+        type: Sequelize.ENUM,
+        values: ['primary', 'secondary'],
+        defaultValue: 'primary',
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contacts');
+  }
+};
---
+++ src/models/contact.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Contact extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Contact.init({
+    phoneNumber: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    linkedId: {
+      type: DataTypes.INTEGER
+    },
+    linkPrecedence: {
+      type: DataTypes.ENUM,
+      values: ['primary', 'secondary'],
+      defaultValue: 'primary',
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Contact',
+  });
+  return Contact;
+};
